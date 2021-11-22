1617775ed5f19755cdafec8baf94d5b0f0bc679f
AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>
2021-11-22 09:40:37 +0700

    Add files via upload

---
+++ Tools-Termux.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+import os
+import time
+import sys
+
+os.system("clear")
+
+print('''\033[91m
+__     ______  ____    ____            _
+\ \   / /  _ \|  _ \  | __ )  __ _ ___(_) ___
+ \ \ / /| |_) | |_) | |  _ \ / _` / __| |/ __|
+  \ V / |  __/|  __/  | |_) | (_| \__ \ | (__
+   \_/  |_|   |_|     |____/ \__,_|___/_|\___|
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+print (''' \033[95m
++--------------------------------------+
+| This Tool Install All Basic Packages |
++--------------------------------------+
+| Author : SUBUR.M  |   version : 2.0  |
++--------------------------------------+''')
+
+slowprint(''' \033[93m
+CHANNEL : Cyber-BasT7
+---------------------
+TIDAK MENGEMIS SUBCRIBE & LIKE 
+ 
+{ HACKERS ABAL ABAL } link : http://bit.ly/2ZUcZvP
+
+[01] python
+[02] python2
+[03] python-dev
+[04] python3
+[05] php
+[06] java
+[07] git
+[08] perl
+[09] bash
+[10] nano
+[11] curl
+[12] openssl
+[13] openssh
+[14] wget
+[15] clang
+[16] nmap
+[17] w3m
+[18] hydra
+[19] ruby
+[20] macchanger
+[21] host
+[22] dnsutils
+[23] coreutils ''')
+slowprint('''\033[96m
+BUAT SCRIPT INI TUH GAK MUDAH BANGSAT.!! , LU TINGGAL GUNAIN AJA GAK USAH REPOT REPOT CUK,GUA KASIH LINK DI BAWAH BUAT LU DOWNLOAD B-SAT
+[00] LINK : https://github.com/SUBUR78990''')
+print ("                                            ")
+choice = input("\033[93mDo You Want to Install All Packages [y/n] : ")
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt update")
+os.system ("atp upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+
+print ("wait for second and start hacking")
+
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+
+
+print ("""
+subscribe VPP Hacker """)
+
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+
+print ("Allow the Button For Access the Storage in Termux")
+
+
+os.system ("termux-setup-storage")
+
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[95m+-------------------------------------------------+")
+slowprint('''\033[95m|             Welcome To Hackers World            |
+|           Subscribe dulu bangs*t : http://bit.ly/2ZUcZvP        |
+| Watch Ours Tutorials For Learn Ethical Hacking  |''')
+print("+-------------------------------------------------+")
+
+input("\n\nPress the enter key to exit : ")
